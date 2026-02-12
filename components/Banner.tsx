f504520cbc2ce0fe4cab83cbb24dd462cc25b34f
88lightningcreations <88lightningcreations@gmail.com>
2026-02-12 02:04:44 +0000

    production, without estimator

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,15 +1,11 @@
 
 import styles from './Banner.module.css';
-import CostEstimator from './CostEstimator';
 
 const Banner = () => {
   return (
     <div className={styles.banner}>
       <div className={styles.textContainer}>
         <h1 className={styles.headline}>Your Vision, Our Expertise</h1>
-      </div>
-      <div className={styles.estimatorContainer}>
-        <CostEstimator />
       </div>
     </div>
   );
